b3668048984c09cd1b26728cda382e7d8a5a52a9
Gage Cates <gagecates10@gmail.com>
2021-01-21 17:09:49 -0700

    update

--- js/app.js
+++ js/app.js
@@ -1,42 +1,46 @@
-const navbarLinks = document.getElementById("navbar__list");
+const navbarList = document.getElementById("navbar__list");
 const sections = document.querySelectorAll("section");
 
 function createNavLinks(){
     for (let section of sections){
         let navLink = document.createElement("li");
-        navLink.className = section.className;
+        navLink.className = "menu__link";
+        navLink.id = section.className;
         navLink.innerText = section.getAttribute('data-nav');
         navLink.addEventListener('click',function(){
-            section.scrollIntoView({behavior: "smooth"});
+            section.scrollIntoView({behavior: "smooth"})
+
         });
 
-        navbarLinks.appendChild(navLink);
+        navbarList.appendChild(navLink);
     }
 }
 createNavLinks();
 
     
-function inViewPort(){
-    const bounding = elem.getBoundingClientRect();
+function inViewPort(section){
+    const bounding = section.getBoundingClientRect();
     return (
         bounding.top >= 0 &&
         bounding.left >= 0 &&
         bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
         bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
+}
     
 function setActive(){
     for (let section of sections){
-        let navLink = document.getElementBy
         if(inViewPort(section)){
             section.classList.add('this-section-active');
         }else {
             section.classList.remove('this-section-active');
         
-        });
+        }
+    }
+
+}
     
     
 document.addEventListener('scroll', function(){
     setActive();
 });
-    
