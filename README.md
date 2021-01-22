3c480487aae252863f44b22293106ea80216bd09
Gage Cates <gagecates10@gmail.com>
2021-01-21 18:14:11 -0700

    Final

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # landing-page
-Udacity Front End Project
+
+## Udacity Front End Nanodegree Project
+
+This project tested my skills in DOM minipulation using JS.
+By creating a dyamic nav bar, the user can scroll the page and see the 
+nav bar animations. They can also click on a nav bar link to be scrolled to 
+the corrisponding section of the page. 
+
+## Stack
+```
+    HTML
+    CSS
+    JavaScript
+```
--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 const navbarList = document.getElementById("navbar__list");
 const sections = document.querySelectorAll("section");
-const navbarLinks = document.getElementsByClassName("menu__link");
 
-
+// create dynamic nav bar links for each section
 function createNavLinks(){
     for (let section of sections){
         let navLink = document.createElement("li");
         navLink.className = "menu__link";
-        navLink.id = section.className;
+        navLink.id = section.id + "-nav";
         navLink.innerText = section.getAttribute('data-nav');
         navLink.addEventListener('click',function(){
             section.scrollIntoView({behavior: "smooth"})
@@ -17,9 +16,11 @@ function createNavLinks(){
         navbarList.appendChild(navLink);
     }
 }
+
 createNavLinks();
 
-    
+
+// checks if section is in viewport
 function inViewPort(section){
     const bounding = section.getBoundingClientRect();
     return (
@@ -29,20 +30,26 @@ function inViewPort(section){
         bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
-    
+
+
+// sets section and corisponding nav bar link to active when in viewport
 function setActive(){
     for (let section of sections){
+        const navLink = document.getElementById(section.id + "-nav");
         if(inViewPort(section)){
             section.classList.add('this-section-active');
+            navLink.classList.add('nav-active');
         }else {
             section.classList.remove('this-section-active');
+            navLink.classList.remove('nav-active');
         
         }
     }
 
 }
     
-    
+
+// every scroll checks what section is in viewport
 document.addEventListener('scroll', function(){
     setActive();
 });
